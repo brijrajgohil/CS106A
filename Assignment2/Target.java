b1ae41dc77e38afa35487d0fcef59c8be66f320f
Brijrajsinh Gohil <brijrajsinhgohil@yahoo.com>
2015-12-12 19:34:09 +0530

    Target Done

--- Assignment2/Target.java
+++ Assignment2/Target.java
@@ -12,6 +12,25 @@ import java.awt.*;
 
 public class Target extends GraphicsProgram {	
 	public void run() {
-		/* You fill this in. */
+		int cx = getWidth()/2;
+		int cy = getHeight()/2;
+		double r = 72;
+		redCircle(cx, cy, r);
+		r = 46.8;
+		whiteCircle(cx, cy, r);
+		r = 21.6;
+		redCircle(cx, cy, r);
+	}
+	private void redCircle(double cx, double cy, double r) {
+		GOval tar = new GOval(cx-r, cy-r, r*2, r*2);
+		tar.setColor(Color.RED);
+		tar.setFilled(true);
+		add(tar);
+	}
+	private void whiteCircle(double cx, double cy, double r) {
+		GOval tar = new GOval(cx-r, cy-r, r*2, r*2);
+		tar.setColor(Color.WHITE);
+		tar.setFilled(true);
+		add(tar);
 	}
 }
