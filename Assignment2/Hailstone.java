49c1dcfa7062b310644277f15686df24aaa32530
Brijrajsinh Gohil <brijrajsinhgohil@yahoo.com>
2015-12-13 00:33:19 +0530

    Done

--- Assignment2/Hailstone.java
+++ Assignment2/Hailstone.java
@@ -10,7 +10,20 @@ import acm.program.*;
 
 public class Hailstone extends ConsoleProgram {
 	public void run() {
-		/* You fill this in */
+		int n = readInt("Enter a number: ");
+		int count = 0;
+		while(n > 1) {
+			if(n%2 == 0) {
+				println(n + " is even, so I take half: " + n/2);
+				n = n/2;
+				count++;
+			} else {
+				println(n + " is odd, so I make 3n+1: " + ((3*n) + 1));
+				n = ((3*n) + 1);
+				count++;
+			}
+		}
+		println("The process took " + count + " to reach 1");
 	}
 }
 
