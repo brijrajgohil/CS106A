e3459192bca43af3fb9c7ca042690d770905c248
Brijrajsinh Gohil <brijrajsinhgohil@yahoo.com>
2016-01-14 10:48:50 +0530

    6.9

--- BlankProject/BlankClass.java
+++ BlankProject/BlankClass.java
@@ -7,44 +7,14 @@
  */
 
 import acm.program.*;
-import acm.util.*;
+
 
 public class BlankClass extends ConsoleProgram {
 	public void run() {
-		int total = rollTwoDice();
-		if(total == 7 || total == 11) {
-			println("That's a natural. You win");
-		} else if(total == 2 || total == 3 || total == 12) {
-			println("That's craps. You lose.");
-		} else {
-			int point = total;
-			println("Your point is " + point);
-			while(true) {
-				if(total == point) {
-					println("You made your point. You win.");
-					break;
-				} else if(total == 7) {
-					println("That a 7. You lose.");
-					break;
-				}
-			}
-		}
-		String coinFlip = rgen.nextBoolean(0.0) ? "HEADS" : "TAILS";
-		println(coinFlip);
+		Stoplight myStoplight = new Stoplight();
+		println(myStoplight.getState());
+		myStoplight.advance();
+		println(myStoplight.getState());
 	}
-	
-	private int rollTwoDice() {
-		//rgen.setSeed(1);
-		int d1 = rgen.nextInt(1, 6);
-		int d2 = rgen.nextInt(1, 6);
-		int total = d1 + d2;
-		println("Rolled dice: " + d1 + " " + d2 + " = " + total);
-		return total;
-	}
-	
-	
-	private RandomGenerator rgen = new RandomGenerator();
-	
-	
 }
 
