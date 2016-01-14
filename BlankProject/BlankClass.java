cfbdf7f0283a0c279ec656024c8359a886168363
Brijrajsinh Gohil <brijrajsinhgohil@yahoo.com>
2016-01-14 12:13:54 +0530

    6.1 PE

--- BlankProject/BlankClass.java
+++ BlankProject/BlankClass.java
@@ -7,14 +7,31 @@
  */
 
 import acm.program.*;
-
+import acm.util.*;
 
 public class BlankClass extends ConsoleProgram {
 	public void run() {
-		Stoplight myStoplight = new Stoplight();
-		println(myStoplight.getState());
-		myStoplight.advance();
-		println(myStoplight.getState());
+	int cardNumber = rgen.nextInt(1, 52);
+	String suit = giveSuit(cardNumber);
+	String rank = giveRank(cardNumber);
+	println(rank + " of " + suit);
 	}
+	
+	private String giveSuit(int cardNumber) {
+		if(cardNumber <= 13) return "Clubs";
+		else if(cardNumber >= 14 && cardNumber <= 26) return "Diamonds";
+		else if(cardNumber >= 27 && cardNumber <= 39) return "Hearts";
+		else return "Spades";
+	}
+	
+	private String giveRank(int cardNumber) {
+			if(cardNumber == 1) return "Ace";
+			else if(cardNumber >= 2 && cardNumber <= 10) return (""+ cardNumber);
+			else if(cardNumber == 11) return "Jack";
+			else if(cardNumber == 12) return "Queen";
+			else return "King";
+	}
+	
+	private RandomGenerator rgen = new RandomGenerator();
 }
 
