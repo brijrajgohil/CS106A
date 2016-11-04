0f17198b5e557b4687cc9fd0fead456bd4f75e3d
Brijrajsinh Gohil <brijrajsinhgohil@yahoo.com>
2016-11-04 09:36:56 +0530

    Its good to be back

--- BlankProject/BlankClass.java
+++ BlankProject/BlankClass.java
@@ -6,26 +6,22 @@
  * Then you can extend GraphicsProgram, ConsoleProgram, or DialogProgram as you like.
  */
 
-import acm.graphics.GRect;
 import acm.program.*;
 import acm.util.*;
 import java.awt.*;
 
-public class BlankClass extends GraphicsProgram {
+public class BlankClass extends ConsoleProgram {
 	public void run() {
-		GRect square = new GRect(0, 0, SQSIZE, SQSIZE);
-		square.setFilled(true);
-		square.setFillColor(Color.RED);
-		add(square);
-		double dx = (getWidth() - SQSIZE) / NSTEPS;
-		double dy = 0;
-		for(int i = 0; i < NSTEPS; i++) {
-			square.move(dx, dy);
-			pause(PAUSE_TIME);
-		}	
+		println("Fib series");
+		int t1 = 0;
+		int t2 = 1;
+		while(t1 <= MAX_TERM_VALUE) {
+			println(t1);
+			int t3 = t1 + t2;
+			t1 = t2;
+			t2 = t3;
+		}
 	}
-	public static final double NSTEPS = 10.0;
-	public static final double SQSIZE = 100;
-	public static final double PAUSE_TIME = 1000;
+	private static final int MAX_TERM_VALUE = 10000;
 }
 
