a117f7a849c2e4c980cb9dbcc1f687d7bb7d3421
Brijrajsinh Gohil <brijrajsinhgohil@yahoo.com>
2016-12-01 11:18:08 +0530

    Update

--- BlankProject/BlankClass.java
+++ BlankProject/BlankClass.java
@@ -9,24 +9,15 @@
 import acm.program.*;
 import acm.util.*;
 
-
 public class BlankClass extends ConsoleProgram {
 	
-	private static final int NDARTS = 10000;
-	
+	private static final int NUM_SIDES = 6;
 	public void run() {
-		int inside = 0;
-		for(int i = 0; i < NDARTS; i++) {
-			double x = rgen.nextDouble(-1.0, +1.0);
-			double y = rgen.nextDouble(-1.0, +1.0);
-			
-			if(((x * x) + (y * y)) < 1.0) {
-				inside++;
-			}
+		int numRolls = readInt("Enter num of rolls ");
+		for(int i = 0; i < numRolls; i++) {
+			int roll = rgen.nextInt(1, NUM_SIDES);
+			println(roll);
 		}
-		double pi = (4.0 * inside) / NDARTS;
-		println("Pi is approx " + pi);
 	}
-	private RandomGenerator rgen = RandomGenerator.getInstance();
-	
+	private RandomGenerator rgen = new RandomGenerator();
 }
