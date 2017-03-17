fe2de60ee87f0359105589cbf31e927baddeef75
Brijrajsinh Gohil <brijrajsinhgohil@yahoo.com>
2017-03-18 00:31:30 +0530

    Array programs

--- BlankProject/BlankClass.java
+++ BlankProject/BlankClass.java
@@ -4,47 +4,41 @@
 import acm.program.*;
 import acm.graphics.*;
 import java.awt.*;
-public class BlankClass extends GraphicsProgram{
+public class BlankClass extends ConsoleProgram {
 	public void run() {
-		initPegArray();
-		int thisPeg = 0;
-		int nextPeg = -1;
-		while (thisPeg != 0 || nextPeg == -1) {
-			nextPeg = (thisPeg + DELTA) % N_PEGS;
-			GPoint p0 = pegs[thisPeg];
-			GPoint p1 = pegs[nextPeg];
-			GLine line = new GLine(p0.getX(), p0.getY(), p1.getX(), p1.getY());
-			line.setColor(Color.MAGENTA);
-			add(line);
-			thisPeg = nextPeg;
+		println("This program counts letter frequencies. ");
+		println("Enter a blank line to indicate the end of the text. ");
+		initFrequencyTable();
+		while(true) {
+			String line = readLine();
+			if(line.length() == 0) break;
+			countLetterFrequencies(line);
+		}
+		printFrequencyTable();
+	}
+	private void initFrequencyTable() {
+		frequencyTable = new int[26];
+		for(int i = 0; i < 26; i++) {
+			frequencyTable[i] = 0;
 		}
 	}
 	
-	private void initPegArray() {
-		int pegIndex = 0;
-		for(int i = 0; i < N_ACROSS; i++) {
-			pegs[pegIndex++] = new GPoint(i * PEG_SEP, 0);
-		}
-		
-		for(int i = 0; i < N_DOWN; i++) {
-			pegs[pegIndex++] = new GPoint(N_ACROSS * PEG_SEP, i * PEG_SEP);
-		}
-		
-		for(int i = N_ACROSS; i > 0; i--) {
-			pegs[pegIndex++] = new GPoint(i * PEG_SEP, N_DOWN * PEG_SEP);
-		}
-		
-		for(int i = N_DOWN; i > 0; i--) {
-			pegs[pegIndex++] = new GPoint(0, i * PEG_SEP);
+	private void countLetterFrequencies(String line) {
+		for(int i = 0; i < line.length(); i++) {
+			char ch = line.charAt(i);
+			if(Character.isLetter(ch)) {
+				int index = Character.toUpperCase(ch) - 'A';
+				frequencyTable[index]++;
+			}
 		}
 	}
 	
-	private static final int DELTA = 67;
-	private static final int PEG_SEP = 10;
-	private static final int N_ACROSS = 50;
-	private static final int N_DOWN = 30;
-	private static final int N_PEGS = 2 * N_ACROSS + 2 * N_DOWN;
+	private void printFrequencyTable() {
+		for(char ch = 'A'; ch <= 'Z'; ch++) {
+			int index = ch - 'A';
+			println(ch + ": " + frequencyTable[index]);
+		}
+	}
 	
-	private GPoint[] pegs = new GPoint[N_PEGS];
-	
+	private int[] frequencyTable;
 }
